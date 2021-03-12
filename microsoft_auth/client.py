a3233ce17f06f1b6c4cd2d69f3d85e3d4c28b3e6
andrewbutlerboudakiankce <51485517+andrewbutlerboudakiankce@users.noreply.github.com>
2021-03-12 15:20:37 -0500

    Update scopes to match existing app
    To avoid needing to create a separate Azure App all together

--- microsoft_auth/client.py
+++ microsoft_auth/client.py
@@ -46,7 +46,7 @@ class MicrosoftClient(OAuth2Session):
 
     # required OAuth scopes
     SCOPE_XBL = ["XboxLive.signin", "XboxLive.offline_access"]
-    SCOPE_MICROSOFT = ["openid", "email", "profile"]
+    SCOPE_MICROSOFT = ["openid", "email", "profile", "User.Read", "User.ReadBasic.All"]
 
     def __init__(self, state=None, request=None, *args, **kwargs):
         from .conf import config
