8fcf0eebe163c6bad45cd1bae73ca6c7c699098c
andrewbutlerboudakiankce <51485517+andrewbutlerboudakiankce@users.noreply.github.com>
2021-03-19 12:24:07 -0400

    remove ptr migration dependency

--- microsoft_auth/migrations/0001_initial.py
+++ microsoft_auth/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL), ('ptr', '0003_site_configuration')]
+    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL)]
 
     operations = [
         migrations.CreateModel(
