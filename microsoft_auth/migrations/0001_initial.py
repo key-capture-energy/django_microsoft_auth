b2d6105159688f5500ed9d6a0bfe120aeabeb3e1
andrewbutlerboudakiankce <51485517+andrewbutlerboudakiankce@users.noreply.github.com>
2021-03-19 12:40:16 -0400

    change dependency to ptr 0001_initial

--- microsoft_auth/migrations/0001_initial.py
+++ microsoft_auth/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL)]
+    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL), ('ptr', '0001_initial')]
 
     operations = [
         migrations.CreateModel(
