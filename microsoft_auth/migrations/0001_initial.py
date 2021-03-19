71ef5e6d2797770eea0222eacade262eb3668bfe
andrewbutlerboudakiankce <51485517+andrewbutlerboudakiankce@users.noreply.github.com>
2021-03-19 12:46:16 -0400

    migration schmigration

--- microsoft_auth/migrations/0001_initial.py
+++ microsoft_auth/migrations/0001_initial.py
@@ -4,12 +4,12 @@
 
 # import django.db.models.deletion
 # from django.conf import settings
-# from django.db import migrations, models
+from django.db import migrations, models
 
 
-# class Migration(migrations.Migration):
+class Migration(migrations.Migration):
 
-#     initial = True
+    initial = True
 
 #     dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL), ('ptr', '0001_initial')]
 
